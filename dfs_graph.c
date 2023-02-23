e29df3f03da44f54d0f5ff941f4a255ae3262ea0
Tribanocci <kostakitsan@gmail.com>
2023-02-23 13:23:33 +0200

    change language

--- dfs_graph.c
+++ dfs_graph.c
@@ -1,9 +1,3 @@
-//--------Onomata-------------ΑΜ----------------
-//--------Eleni_Frantzi-------2020010133--------
-//--------Elisavet_Manali-----2020010087--------
-//--------Rafaillia_Platinaki-2020010126--------
-//--------Rodopi_Viltanioti---2020010117--------
-//----------------------------------------------
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,8 +8,8 @@
 
 int main(void)
 {//-------------INPUT---------------------------------------
-	// dilosi akmon
-	Edge akmes[] =
+	// Edges Declaration
+	Edge my_edge[] =
 	{
 		{1, 2}, {1, 3}, {2, 3}, {2, 4}, {4, 3}, {4, 5}, 
 		{5, 6}, {6, 7}, {6, 8}, {7, 5}, {7, 8}
@@ -24,69 +18,69 @@ int main(void)
 //-----------------------------------------------------------	
 	
 	
-	// ypologismos plithous akmon
-	int Plith_akmon = sizeof(akmes)/sizeof(akmes[0]);
+	// Number of edges
+	int Number_edges = sizeof(my_edge)/sizeof(my_edge[0]);
 
-	//dhmiourgia graphou apo thn domi sto graph.h
-	GraphPtr graph = createGraph(akmes, Plith_akmon);
+	//Graph construction using graph declaration from graph.h
+	GraphPtr graph = createGraph(my_edge, Number_edges);
 
-	// ektiposi graphou
-	printf("\n \t  ARXIKO graphima \n\n");
-	printGraph(graph, Plith_akmon);
+	// Graph print
+	printf("\n \t  INITIAL graph \n\n");
+	printGraph(graph, Number_edges);
 	
 //------------prospelasi DFS----------------------
 	
-	Stack node_stack; //Dimiourgia stoivas (apo mathima domes dedomenon)
+	Stack node_stack; //Stack initialization
 	init_stack(&node_stack);
 	int dfnr[Node_Max] = {0};
-	int theta[Node_Max]= {0}; // To 0 simainei oti o komvos den exei eksereunithei akoma
+	int theta[Node_Max]= {0}; // 0: means that node is not visited yet
 	int visit_num=1;
 	
-	Edge dfs_akmes[Plith_akmon];
-	int dfs_akmes_num = 0;
+	Edge dfs_edge[Number_edges];
+	int dfs_edge_num = 0;
 
-	int i = 1;//eisagogi protou komvoy stin stoiva
+	int i = 1;//insert first node in stack (source node, is node number 1)
 	NodePtr ptr = graph->head[i];
 	dfnr[i]=visit_num;
 	visit_num++;
 	push(&node_stack, i);//----------------------
 	
-	while (!isempty(node_stack)) {//oso iparxoun nodes tin stoiva
-		if (ptr != NULL) {  //an o komvos exi epomeno geitona
-			int next = ptr->label; //epomenos geitons
-			if (dfnr[next] == 0) {  //ean o epomenos geitones DEN exei episkepi apo prin
-				dfnr[next] = visit_num; //episkepsi
-				dfs_akmes[dfs_akmes_num].src =  i; //---------
-				dfs_akmes[dfs_akmes_num].dest =  next; //--prosthesi akmis sto DFS ypographima
-				dfs_akmes_num++;
+	while (!isempty(node_stack)) {//as long as the stack is not empty
+		if (ptr != NULL) {  //if the node has neightbour 
+			int next = ptr->label; //next neightbour
+			if (dfnr[next] == 0) {  //if next neightbour has NOT been visited
+				dfnr[next] = visit_num; //visit
+				dfs_edge[dfs_edge_num].src =  i; //---------
+				dfs_edge[dfs_edge_num].dest =  next; //--add edge to DFS sub-Graph
+				dfs_edge_num++;
 				visit_num++;
-				theta[next] = i; // simiosi komvou proelesis tis episkepsis
-				i = next; //o komvos pou episkeftikame ginetai o energos komvos
-				push(&node_stack, i); //eisagosi energou komvou sthn stoiva
-				ptr = graph->head[i]; //deixnoume stin lista me tous geitones tou energou komvou
+				theta[next] = i; // note the node from which the visit was made
+				i = next; //the visited node is now the active node
+				push(&node_stack, i); //push active node in stack
+				ptr = graph->head[i]; //pointer, points to the connected list of the neightbours of the active node
 			}
-			else { // ean o epomenos geitones EXEI episkepi apo prin
-				ptr = ptr->next; //koitame ton epomeno geitona
+			else { // if next neightbour has BEEN VISITED BEFORE
+				ptr = ptr->next; //look next neightbour
 			}
 		}
-		else {   //ean o komvos DEN exei epomeno geitona (pou na min exi episkeftei apo prin)
-			pop(&node_stack, &i); //afairesi tou komvo apo tin stoiva
-			if (isempty(node_stack)) {break;} //an den iparxoun pleon alloi komvoi sthn stoiva TERMATISMOS
-			i = node_stack.stack_array[node_stack.top]; // o active komvos einai tora o teleutaios komvos tis stoivas
-			ptr = graph->head[i]; //deixnoume stous geitones tou active komvou
+		else {   //if there is no next neightbour(that is un-visited)
+			pop(&node_stack, &i); //delete active node from stack
+			if (isempty(node_stack)) {break;} //if stack is empty exit loop
+			i = node_stack.stack_array[node_stack.top]; // active node is the one that is on the top of th stack
+			ptr = graph->head[i]; //pointer, points to the connected list of the neightbours of the active node
 		}
 	}
-		printf("\n----APOTELESMATA ALGORITHMOU DFS------\n");
-	printf(" Seira episkepis  \t   Apo ton komvo theta \n");
+		printf("\n----RESULTS OF DFS ALGORITHM------\n");
+	printf(" Visiting Sequence  \t   From node theta \n");
 	for (int i=1; i<Node_num+1; i++){
 		printf(" dfnr[ %d ] =  %d \t \t theta[ %d ] =  %d \n", i, dfnr[i], i, theta[i]);
 	}
 	
 	
-	printf("\n \t \t DFS ypo graphima \n\n");
+	printf("\n \t \t DFS sub-Graph \n\n");
 
-	GraphPtr dfs_graph = createGraph(dfs_akmes, dfs_akmes_num);
-	printGraph(dfs_graph, dfs_akmes_num);
+	GraphPtr dfs_graph = createGraph(dfs_edge, dfs_edge_num);
+	printGraph(dfs_graph, dfs_edge_num);
 	
 	return 0;
 }
